ea11942e36b20c8de8c2233f08695bc843353d39
Mirko Da Corte <mirko@etherna.io>
2020-09-28 03:07:41 +0200

    First implementation commit

---
+++ src/EthernaAuthentication/Consts/DefaultClaimTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace Etherna.Authentication.Consts
+{
+    public static class DefaultClaimTypes
+    {
+        public const string ClientId = "client_id";
+        public const string EtherAddress = "ether_address";
+        public const string EtherPreviousAddress = "ether_prev_addresses";
+    }
+}
---
+++ src/EthernaAuthentication/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,74 @@
+﻿using Etherna.Authentication.Consts;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Etherna.Authentication.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string GetClientId(this ClaimsPrincipal user)
+        {
+            if (user is null)
+                throw new ArgumentNullException(nameof(user));
+
+            return user.Claims.First(claim => claim.Type == DefaultClaimTypes.ClientId).Value;
+        }
+
+        public static string GetEtherAddress(this ClaimsPrincipal user)
+        {
+            if (user is null)
+                throw new ArgumentNullException(nameof(user));
+
+            return user.Claims.First(claim => claim.Type == DefaultClaimTypes.EtherAddress).Value;
+        }
+
+        public static string[] GetEtherPrevAddresses(this ClaimsPrincipal user)
+        {
+            if (user is null)
+                throw new ArgumentNullException(nameof(user));
+
+            var claim = user.Claims.First(claim => claim.Type == DefaultClaimTypes.EtherPreviousAddress);
+            return JsonSerializer.Deserialize<string[]>(claim.Value);
+        }
+
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public static string? TryGetClientId(this ClaimsPrincipal user)
+        {
+            if (user is null) return null;
+
+            var claim = user.Claims.FirstOrDefault(claim => claim.Type == DefaultClaimTypes.ClientId);
+            return claim?.Value;
+        }
+
+        public static string? TryGetEtherAddress(this ClaimsPrincipal user)
+        {
+            if (user is null) return null;
+
+            var claim = user.Claims.FirstOrDefault(claim => claim.Type == DefaultClaimTypes.EtherAddress);
+            return claim?.Value;
+        }
+
+        public static string[]? TryGetEtherPrevAddresses(this ClaimsPrincipal user)
+        {
+            if (user is null) return null;
+
+            var claim = user.Claims.FirstOrDefault(claim => claim.Type == DefaultClaimTypes.EtherPreviousAddress);
+            return claim is null ? null : JsonSerializer.Deserialize<string[]>(claim.Value);
+        }
+
+#pragma warning disable IDE0060 // Remove unused parameter
+        public static string? TryGetUsername(this ClaimsPrincipal user)
+        {
+            //TODO
+            return null;
+        }
+#pragma warning restore IDE0060 // Remove unused parameter
+    }
+}
