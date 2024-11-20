0b1592e0d8dde7187919b757cad136f104dbbfc1
Mirko Da Corte <mirko@etherna.io>
2024-11-20 12:01:25 +0100

    update to dotnet 9

--- src/EthernaAuthentication/EthernaClaimTypes.cs
+++ src/EthernaAuthentication/EthernaClaimTypes.cs
@@ -23,8 +23,10 @@ namespace Etherna.Authentication
         public const string EtherAddress = "ether_address";
         public const string EtherPreviousAddresses = "ether_prev_addresses";
         public const string IsWeb3Account = "isWeb3Account";
+#pragma warning disable CA1707
         public const string Role_Dotnet = ClaimTypes.Role;
         public const string Role_IdentityModel = JwtClaimTypes.Role;
+#pragma warning restore CA1707
         public const string UserId = JwtClaimTypes.Subject;
         public const string Username = "preferred_username";
     }
