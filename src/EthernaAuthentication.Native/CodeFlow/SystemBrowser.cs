e6e55586eddb8d8ec93017ce9ac8e8814d822c37
Mirko Da Corte <mirko@etherna.io>
2026-07-06 21:42:22 +0200

    handle exceptions opening browser

--- src/EthernaAuthentication.Native/CodeFlow/SystemBrowser.cs
+++ src/EthernaAuthentication.Native/CodeFlow/SystemBrowser.cs
@@ -1,14 +1,14 @@
-﻿// Copyright 2021-present Etherna SA
+// Copyright 2021-present Etherna SA
 // This file is part of EthernaAuthentication.
-// 
+//
 // EthernaAuthentication is free software: you can redistribute it and/or modify it under the terms of the
 // GNU Lesser General Public License as published by the Free Software Foundation,
 // either version 3 of the License, or (at your option) any later version.
-// 
+//
 // EthernaAuthentication is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License along with EthernaAuthentication.
 // If not, see <https://www.gnu.org/licenses/>.
 
@@ -17,43 +17,26 @@ using System;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Etherna.Authentication.Native.CodeFlow
 {
-    internal sealed class SystemBrowser : IBrowser
+    internal sealed class SystemBrowser(
+        int? port = null,
+        string? path = null,
+        string? customFailureContentType = null,
+        string? customFailureResponse = null,
+        string? customSuccessContentType = null,
+        string? customSuccessResponse = null,
+        Action<string>? openBrowser = null)
+        : IBrowser
     {
         // Fields.
-        private readonly string? customFailureContentType;
-        private readonly string? customFailureResponse;
-        private readonly string? customSuccessContentType;
-        private readonly string? customSuccessResponse;
-        private readonly string path;
-
-        // Constructor.
-        public SystemBrowser(
-            int? port = null,
-            string? path = null,
-            string? customFailureContentType = null,
-            string? customFailureResponse = null,
-            string? customSuccessContentType = null,
-            string? customSuccessResponse = null)
-        {
-            this.customFailureContentType = customFailureContentType;
-            this.customFailureResponse = customFailureResponse;
-            this.customSuccessContentType = customSuccessContentType;
-            this.customSuccessResponse = customSuccessResponse;
-            this.path = path ?? string.Empty;
-            if (!port.HasValue)
-                Port = GetRandomUnusedPort();
-            else
-                Port = port.Value;
-        }
+        private readonly Action<string> openBrowser = openBrowser ?? OpenBrowser;
 
         // Properties.
-        public int Port { get; }
+        public int Port { get; } = port ?? GetRandomUnusedPort();
 
         // Methods.
         public async Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken)
@@ -68,9 +51,21 @@ namespace Etherna.Authentication.Native.CodeFlow
                 customSuccessContentType,
                 customSuccessResponse);
 
-            OpenBrowser(options.StartUrl);
-
 #pragma warning disable CA1031 // Do not catch general exception types
+            try
+            {
+                openBrowser(options.StartUrl);
+            }
+            catch (Exception ex)
+            {
+                return new BrowserResult
+                {
+                    ResultType = BrowserResultType.UnknownError,
+                    Error = $"Failed to open the system browser: {ex.Message}. " +
+                        "If a browser is not available in this environment, sign in with an API key instead."
+                };
+            }
+
             try
             {
                 var result = await listener.WaitForCallbackAsync().ConfigureAwait(false);
@@ -91,29 +86,12 @@ namespace Etherna.Authentication.Native.CodeFlow
         }
 
         // Static methods.
-        public static void OpenBrowser(string? url)
-        {
-            url ??= string.Empty;
-            try
+        public static void OpenBrowser(string url) =>
+            Process.Start(new ProcessStartInfo(url)
             {
-                Process.Start(url);
-            }
-            catch
-            {
-                // hack because of this: https://github.com/dotnet/corefx/issues/10361
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    url = url.Replace("&", "^&", StringComparison.InvariantCultureIgnoreCase);
-                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                    Process.Start("xdg-open", url);
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                    Process.Start("open", url);
-                else
-                    throw;
-            }
-        }
+                //required to open a url with the OS default browser (cmd on Windows, xdg-open on Linux, open on macOS)
+                UseShellExecute = true
+            });
 
         // Helpers.
         private static int GetRandomUnusedPort()
